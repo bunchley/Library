f971d0089230d7727d0c340c39181efbf0353bea
bunchley <bunchley007@gmail.com>
2019-12-31 17:45:21 -0700

    fixed delete button bug

---
+++ notes.js
@@ -0,0 +1,8 @@
+//creating a neww object
+let obj = {};
+let nextObj = Object.create(Object.prototype);
+let lastObj = new Object();
+
+//use dot notation to assign keys and values
+// use square bracket notation to assign keys and values
+
--- script/Library.js
+++ script/Library.js
@@ -21,7 +21,11 @@ function addBookToLibrary() {
   let author = document.querySelector("[data-name=author]").value;
   let pages = document.querySelector("[data-name=page]").value;
   let haveRead = document.querySelector("[data-name=read]").checked;
-  if (haveRead ? (haveRead = "Yes") : (haveRead = "No"))
+  if (
+    haveRead === true
+      ? (haveRead = "Already read")
+      : (haveRead = "Want to Read")
+  )
     if ((title === "") | (author === "") | (pages === "")) {
       //provide an error
       console.log(
@@ -57,28 +61,35 @@ closeButton.addEventListener("click", function(e) {
 
 //delete row button activation function
 libraryContainer.addEventListener("click", e => {
+  if (e.target.classList.contains("delete")) {
     e.preventDefault();
-    console.log('target infor', e.target.getAttribute('data-num'));
-    let buttonArrayPosition = e.target.getAttribute('data-num');
-    myLibrary.splice(myLibrary.indexOf(buttonArrayPosition), 1)
+    console.log("target infor", e.target.getAttribute("data-num"));
+    let buttonArrayPosition = e.target.getAttribute("data-num");
+    myLibrary.splice(myLibrary.indexOf(buttonArrayPosition), 1);
     console.log(myLibrary[buttonArrayPosition]);
     render();
-  });
-
+  } else if (e.target.classList.contains("read-status")) {
+    let buttonArrayPosition = e.target.getAttribute("data-num");
+    //myLibrary[buttonArrayPosition].haveRead   <______________________where you left off.
+  }
+});
 
 //display Library
 function render() {
   let tableElement = "<tbody>";
-  let i=0;
+  let i = 0;
   for (; i < myLibrary.length; i++) {
     tableElement += "<tr>";
     tableElement += "<td>" + myLibrary[i].title + "</td>";
     tableElement += "<td>" + myLibrary[i].author + "</td>";
     tableElement += "<td>" + myLibrary[i].pages + "</td>";
-    tableElement += "<td>" + myLibrary[i].haveRead + "</td>";
-    tableElement += "<td>" + `<button class='delete' data-num=${i}>Delete</button>` + "</td>";
+    tableElement +=
+      "<td>" +
+      `<button class='read-status' data-num=${i}>${myLibrary[i].haveRead}</button>` +
+      "</td>";
+    tableElement +=
+      "<td>" + `<button class='delete' data-num=${i}>Delete</button>` + "</td>";
     tableElement += "</tr>";
-    
   }
   tableElement += "</tbody>";
   libraryContainer.innerHTML = tableElement;
