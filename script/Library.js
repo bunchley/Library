ff1b8f9f426c74e5eb8c66f98253971ce756f94e
Ashley.Bunch <ashley.bunch@asynchrony.com>
2021-05-24 16:27:59 -0600

    fixed delete bug and removed console logs

--- script/Library.js
+++ script/Library.js
@@ -1,12 +1,13 @@
 "use strict";
 let myLibrary = [
-  new Book("Crime and Punnishment", "Dostoevsky", "878", "Want to Read")
+  new Book("Crime and Punnishment", "Dostoevsky", "878", "Want to Read"),
 ];
 
 const form = document.querySelector(".add-form");
 const libraryContainer = document.querySelector(".library-books");
 const newBookFormContainer = document.querySelector(".top-half");
 
+//constructr below
 function Book(title, author, pages, haveRead) {
   this.title = title;
   this.author = author;
@@ -14,7 +15,7 @@ function Book(title, author, pages, haveRead) {
   this.haveRead = haveRead;
 }
 
-Book.prototype.changeReadStatus = function(newStatus) {
+Book.prototype.changeReadStatus = function (newStatus) {
   this.read = newStatus;
 };
 
@@ -40,49 +41,41 @@ function addBookToLibrary() {
     }
 }
 function toggleReadBook(id) {
-  console.log("in the read toggler");
   myLibrary[id].read === true ? false : true;
 }
 
-newBookFormContainer.addEventListener("click", e => {
+newBookFormContainer.addEventListener("click", (e) => {
   //prevents a reload
   e.preventDefault();
   //display the add book to library form
   if (e.target.classList.contains("add-button")) {
-    console.log("add button selected");
-
     form.style.display = "block";
   }
   //trigger adding book to array
   else if (e.target.classList.contains("addNewBook")) {
-    console.log("add book button clicked");
     addBookToLibrary();
     render();
   }
   //close add new book form
   else if (e.target.classList.contains("close")) {
-    console.log("close button selected");
     form.style.display = "none";
   }
 });
 
 //delete and read status handler for library
-libraryContainer.addEventListener("click", e => {
+libraryContainer.addEventListener("click", (e) => {
   //prevents a reload
   e.preventDefault();
 
   //delete button lsitener
   if (e.target.classList.contains("delete")) {
-    console.log("target infor", e.target.getAttribute("data-num"));
     let buttonArrayPosition = e.target.getAttribute("data-num");
-    myLibrary.splice(myLibrary.indexOf(buttonArrayPosition), 1);
-    console.log(myLibrary[buttonArrayPosition]);
+    myLibrary.splice(buttonArrayPosition, 1);
     render();
   }
   //read status toggler
   else if (e.target.classList.contains("read-status")) {
     let buttonArrayPosition = e.target.getAttribute("data-num");
-    console.log("read status clicked");
     if (e.target.innerText === "Want to Read") {
       myLibrary[buttonArrayPosition].changeReadStatus("Read");
     } else {
